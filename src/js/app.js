046f8a076d0eed7e6b9f94d8f180a967bcceb97f
Ruf <raffaatha03@gmail.com>
2026-09-20 00:39:33 +0700

    feat: added delete completed task button

--- src/js/app.js
+++ src/js/app.js
@@ -187,6 +187,13 @@ function edittask(id, newText) {
 // Buat function clearCompleted() yang menghapus semua task dengan
 // completed === true dari array "tasks", lalu panggil renderTasks().
 // Jangan lupa tambahkan event listener untuk tombol #clear-completed.
+function clearCompleted() {
+  tasks = tasks.filter((task) => !task.completed);
+  renderTasks();
+}
+
+const delCompleted = document.querySelector(".del-completed");
+delCompleted.addEventListener("click", clearCompleted());
 
 // TODO (Fitur #3 - Filter Task):
 // Simpan filter yang sedang aktif di sebuah variabel, misalnya
